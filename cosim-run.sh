061aace9c272566a11404c9800eb26626991dcad
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-06-19 22:47:05 +0200

    add cosim-run script

---
+++ cosim-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ "$COSIM_PATH" = "" ] ; then
+  COSIM_PATH="/local/endhostsim/ehsim"
+fi
+
+export CPPFLAGS="-I$COSIM_PATH/proto -I$COSIM_PATH/netsim_common/include"
+export LDFLAGS="-L$COSIM_PATH/netsim_common/ -lnetsim_common"
+
+DIR="$(dirname "$(readlink -f "$0")")"
+cd $DIR
+
+./waf --run "src/cosim/examples/$*"
+#./waf --run "src/cosim/examples/cosim-bridge-example" --command-template="gdb --args %s --CosimPort=/local/endhostsim/ehsim/experiments/out/qemu-ns3-bridge-pair/eth.a --CosimPort=/local/endhostsim/ehsim/experiments/out/qemu-ns3-bridge-pair/eth.b"
