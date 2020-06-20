bc60da7f397d83b44aa5e6d2ee37774b61e7b5ca
Antoine Kaufmann <antoinek@mpi-sws.org>
2020-06-20 17:11:37 +0200

    cosim: run script that uses exec to have correct pids for killing

--- cosim-run.sh
+++ cosim-run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-if [ "$COSIM_PATH" = "" ] ; then
-  COSIM_PATH="/local/endhostsim/ehsim"
-fi
-
-export CPPFLAGS="-I$COSIM_PATH/proto -I$COSIM_PATH/netsim_common/include"
-export LDFLAGS="-L$COSIM_PATH/netsim_common/ -lnetsim_common"
-
 DIR="$(dirname "$(readlink -f "$0")")"
 cd $DIR
 
-./waf --run "src/cosim/examples/$*"
-#./waf --run "src/cosim/examples/cosim-bridge-example" --command-template="gdb --args %s --CosimPort=/local/endhostsim/ehsim/experiments/out/qemu-ns3-bridge-pair/eth.a --CosimPort=/local/endhostsim/ehsim/experiments/out/qemu-ns3-bridge-pair/eth.b"
-#./waf --run "src/cosim/examples/cosim-dumbbell-example" --command-template="gdb --args %s --CosimPortLeft=/local/endhostsim/ehsim/experiments/out/qemu-ns3-dumbbell-pair/eth.a --CosimPortRight=/local/endhostsim/ehsim/experiments/out/qemu-ns3-dumbbell-pair/eth.b"
+export LD_LIBRARY_PATH="$DIR/build/lib/:$LD_LIBRARY_PATH"
+
+example=$1
+shift
+
+exec build/src/cosim/examples/ns3-dev-$example-debug "$@"
