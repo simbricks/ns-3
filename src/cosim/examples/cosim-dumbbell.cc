32a1ef64167e5506b5d38eec22b9052863fdd7ef
Hejing Li <hejingli@brain21.mpi-sws.org>
2020-10-21 10:02:06 +0200

    add redqueue for ecn marking

--- src/cosim/examples/cosim-dumbbell.cc
+++ src/cosim/examples/cosim-dumbbell.cc
@@ -17,25 +17,29 @@
  */
 
 #include <iostream>
+#include <iomanip>
 
 #include "ns3/abort.h"
 #include "ns3/core-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-apps-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/traffic-control-module.h"
 #include "ns3/ipv4-static-routing-helper.h"
 #include "ns3/ipv4-list-routing-helper.h"
 #include "ns3/bridge-net-device.h"
 #include "ns3/simple-channel.h"
 #include "ns3/simple-net-device.h"
 #include "ns3/cosim.h"
-
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("CosimDumbbellExample");
 
 std::vector<std::string> cosimLeftPaths;
 std::vector<std::string> cosimRightPaths;
+
+std::ofstream LeftQueueLength;
 
 bool AddCosimLeftPort (std::string arg)
 {
@@ -49,6 +53,17 @@ bool AddCosimRightPort (std::string arg)
   return true;
 }
 
+void
+CheckLeftQueueSize (Ptr<QueueDisc> queue)
+{
+  // 1500 byte packets
+  uint32_t qSize = queue->GetNPackets ();
+  Time backlog = Seconds (static_cast<double> (qSize * 1500 * 8) / 1e10); // 10 Gb/s
+  // report size in units of packets and ms
+  LeftQueueLength << std::fixed << std::setprecision (9) << Simulator::Now ().GetSeconds () << " " << qSize << " " << backlog.GetMicroSeconds () << std::endl;
+  // check queue size every 1/100 of a second
+  Simulator::Schedule (MilliSeconds (100), &CheckLeftQueueSize, queue);
+}
 
 int
 main (int argc, char *argv[])
@@ -67,14 +82,46 @@ main (int argc, char *argv[])
       MakeCallback (&AddCosimRightPort));
   cmd.Parse (argc, argv);
 
-  /*LogComponentEnable("CosimNetDevice", LOG_LEVEL_ALL);
-  LogComponentEnable("BridgeNetDevice", LOG_LEVEL_ALL);*/
+  LogComponentEnable("CosimNetDevice", LOG_LEVEL_ALL);
+  //LogComponentEnable("BridgeNetDevice", LOG_LEVEL_ALL);
+  //LogComponentEnable("CosimDumbbellExample", LOG_LEVEL_ALL);
+  //LogComponentEnable("SimpleChannel", LOG_LEVEL_ALL);
+  LogComponentEnable("SimpleNetDevice", LOG_LEVEL_ALL);
+  //LogComponentEnable ("RedQueueDisc", LOG_LEVEL_ALL);
+
+  //LogComponentEnableAll(LOG_PREFIX_TIME);
+  //LogComponentEnableAll(LOG_PREFIX_NODE);
 
   GlobalValue::Bind ("ChecksumEnabled", BooleanValue (true));
 
+// Set default parameters for RED queue disc
+  Config::SetDefault ("ns3::RedQueueDisc::UseEcn", BooleanValue (true));
+  Config::SetDefault ("ns3::RedQueueDisc::UseHardDrop", BooleanValue (false));
+  Config::SetDefault ("ns3::RedQueueDisc::MeanPktSize", UintegerValue (1500));
+  Config::SetDefault ("ns3::RedQueueDisc::MaxSize", QueueSizeValue (QueueSize ("2666p")));
+// DCTCP tracks instantaneous queue length only; so set QW = 1
+  Config::SetDefault ("ns3::RedQueueDisc::QW", DoubleValue (1));
+  Config::SetDefault ("ns3::RedQueueDisc::MinTh", DoubleValue (1));
+  Config::SetDefault ("ns3::RedQueueDisc::MaxTh", DoubleValue (2));
+  
+
+  TrafficControlHelper tchRed10;
+  // MinTh = 50, MaxTh = 150 recommended in ACM SIGCOMM 2010 DCTCP Paper
+  // This yields a target (MinTh) queue depth of 60us at 10 Gb/s
+  tchRed10.SetRootQueueDisc ("ns3::RedQueueDisc",
+                             "LinkBandwidth", DataRateValue(linkRate),
+                             "LinkDelay", TimeValue (linkLatency),
+                             "MinTh", DoubleValue (1),
+                             "MaxTh", DoubleValue (2));
+
+
+  
   NS_LOG_INFO ("Create Nodes");
   Ptr<Node> nodeLeft = CreateObject<Node> ();
   Ptr<Node> nodeRight = CreateObject<Node> ();
+  NodeContainer nodes (nodeLeft);
+  nodes.Add(nodeRight);
+  NS_LOG_INFO ("Node Num: " << nodes.GetN());
 
   NS_LOG_INFO ("Create BridgeDevice");
   Ptr<BridgeNetDevice> bridgeLeft = CreateObject<BridgeNetDevice> ();
@@ -86,23 +133,48 @@ main (int argc, char *argv[])
 
   NS_LOG_INFO ("Create simple channel link between the two");
   Ptr<SimpleChannel> ptpChan = CreateObject<SimpleChannel> ();
-  ptpChan->SetAttribute ("Delay", TimeValue (linkLatency));
+  //Ptr<PointToPointChannel> ptpChan = CreateObject<PointToPointChannel> ();
+  
+  //PointToPointHelper pointToPointSR;
+  SimpleNetDeviceHelper pointToPointSR;
+  pointToPointSR.SetQueue("ns3::DropTailQueue", "MaxSize", StringValue("1p"));
+  pointToPointSR.SetDeviceAttribute ("DataRate", DataRateValue(linkRate));
+  pointToPointSR.SetChannelAttribute ("Delay", TimeValue (linkLatency));
 
-  Ptr<SimpleNetDevice> ptpDevLeft = CreateObject<SimpleNetDevice> ();
-  Ptr<SimpleNetDevice> ptpDevRight = CreateObject<SimpleNetDevice> ();
-  ptpDevLeft->SetAttribute ("DataRate", DataRateValue(linkRate));
-  ptpDevRight->SetAttribute ("DataRate", DataRateValue(linkRate));
-  ptpDevLeft->SetAddress (Mac48Address::Allocate ());
-  ptpDevRight->SetAddress (Mac48Address::Allocate ());
-  ptpChan->Add (ptpDevLeft);
-  ptpChan->Add (ptpDevRight);
-  ptpDevLeft->SetChannel (ptpChan);
-  ptpDevRight->SetChannel (ptpChan);
-  nodeLeft->AddDevice (ptpDevLeft);
-  nodeRight->AddDevice (ptpDevRight);
-  bridgeLeft->AddBridgePort (ptpDevLeft);
-  bridgeRight->AddBridgePort (ptpDevRight);
+  //ptpChan->SetAttribute ("Delay", TimeValue (linkLatency));
 
+  //Ptr<SimpleNetDevice> ptpDevLeft = CreateObject<SimpleNetDevice> ();
+  //Ptr<SimpleNetDevice> ptpDevRight = CreateObject<SimpleNetDevice> ();
+
+  //Ptr<PointToPointNetDevice> ptpDevLeft = CreateObject<PointToPointNetDevice> ();
+  //Ptr<PointToPointNetDevice> ptpDevRight = CreateObject<PointToPointNetDevice> ();
+
+  //ptpDevLeft = pointToPointSR.Install (nodeLeft, nodeRight).Get(0);
+  //ptpDevRight = pointToPointSR.Install (nodeLeft, nodeRight).Get(1);
+  //NetDeviceContainer ptpDev = pointToPointSR.Install (nodeLeft, nodeRight);
+  NetDeviceContainer ptpDev = pointToPointSR.Install (nodes, ptpChan);
+
+  InternetStackHelper stack;
+  stack.InstallAll ();
+
+  QueueDiscContainer queueDiscs1 = tchRed10.Install (ptpDev);
+
+  //ptpDevLeft->SetAttribute ("DataRate", DataRateValue(linkRate));
+  //ptpDevRight->SetAttribute ("DataRate", DataRateValue(linkRate));
+  //ptpDevLeft->SetAddress (Mac48Address::Allocate ());
+  //ptpDevRight->SetAddress (Mac48Address::Allocate ());
+  //ptpChan->Add (ptpDevLeft);
+  //ptpChan->Add (ptpDevRight);
+  //ptpDevLeft->SetChannel (ptpChan);
+  //ptpDevRight->SetChannel (ptpChan);
+  //nodeLeft->AddDevice (ptpDevLeft);
+  //nodeRight->AddDevice (ptpDevRight);
+  //bridgeLeft->AddBridgePort (ptpDevLeft);
+  //bridgeRight->AddBridgePort (ptpDevRight);
+  NS_LOG_INFO ("num node device" << nodeLeft->GetNDevices() << " type: ");
+  bridgeLeft->AddBridgePort (nodeLeft->GetDevice(1));
+  
+  bridgeRight->AddBridgePort (nodeRight->GetDevice(1));
 
   NS_LOG_INFO ("Create CosimDevices and add them to bridge");
   for (std::string cpp : cosimLeftPaths) {
@@ -119,6 +191,12 @@ main (int argc, char *argv[])
     bridgeRight->AddBridgePort (device);
     device->Start ();
   }
+
+  //AsciiTraceHelper ascii;
+  //pointToPointSR.EnableAsciiAll (ascii.CreateFileStream ("cosim.tr"));
+  //pointToPointSR.EnablePcap ("cosim", ptpDev, 0);
+  LeftQueueLength.open ("cosim-left-length.dat", std::ios::out);
+  Simulator::Schedule (Seconds (0.0), &CheckLeftQueueSize, queueDiscs1.Get (0));
 
   NS_LOG_INFO ("Run Emulation.");
   Simulator::Run ();
