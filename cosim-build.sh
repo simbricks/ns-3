ed6d2d78b50ed71169408fe4f3f34bd324e7da88
Antoine Kaufmann <antoinek@mpi-sws.org>
2021-01-14 21:43:44 +0100

    cosim: update paths for new repo structure

--- cosim-build.sh
+++ cosim-build.sh
@@ -3,8 +3,8 @@ if [ "$COSIM_PATH" = "" ] ; then
   COSIM_PATH="/local/endhostsim/ehsim"
 fi
 
-export CPPFLAGS="-I$COSIM_PATH/proto -I$COSIM_PATH/netsim_common/include"
-export LDFLAGS="-L$COSIM_PATH/netsim_common/ -lnetsim_common"
+export CPPFLAGS="-I$COSIM_PATH/lib/proto -I$COSIM_PATH/lib/netif/include"
+export LDFLAGS="-L$COSIM_PATH/lib/netif/ -lnetsim_common"
 if [ "$1" = "configure" ] ; then
   ./waf configure --enable-examples
 fi
