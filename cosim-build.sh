2b4ffc37098be1408a86eaca0468d53af637160e
Hejing Li <hajeongee@gmail.com>
2022-05-23 22:37:20 +0200

    fix nicif library name

--- cosim-build.sh
+++ cosim-build.sh
@@ -4,7 +4,7 @@ if [ "$COSIM_PATH" = "" ] ; then
 fi
 
 export CPPFLAGS="-I$COSIM_PATH/lib"
-export LDFLAGS="-L$COSIM_PATH/lib/simbricks/nicif/ -lnicif_common -L$COSIM_PATH/lib/simbricks/network/ -lnetwork -L$COSIM_PATH/lib/simbricks/base/ -lbase"
+export LDFLAGS="-L$COSIM_PATH/lib/simbricks/nicif/ -lnicif -L$COSIM_PATH/lib/simbricks/network/ -lnetwork -L$COSIM_PATH/lib/simbricks/base/ -lbase"
 if [ "$1" = "configure" ] ; then
   ./waf configure --enable-examples
 fi
