a7a5cc79e1f5a5372413e2b01845a1afab16637d
Jialin Li <lijl@comp.nus.edu.sg>
2022-06-05 12:47:37 +0800

    suppress compiler warning

--- cosim-build.sh
+++ cosim-build.sh
@@ -5,6 +5,7 @@ fi
 
 export CPPFLAGS="-I$COSIM_PATH/lib"
 export LDFLAGS="-L$COSIM_PATH/lib/simbricks/netif/ -lnetif_common -L$COSIM_PATH/lib/simbricks/nicif/ -lnicif_common"
+export CXXFLAGS="-Wno-range-loop-construct"
 if [ "$1" = "configure" ] ; then
   ./waf configure --enable-examples
 fi
