25f1bc5b2dce5b6b2038347f1efd0e4a7e37e3d0
Jialin Li <lijl@comp.nus.edu.sg>
2022-06-06 00:09:16 +0800

    suppress compiler warning for new gcc versions

--- cosim-build.sh
+++ cosim-build.sh
@@ -5,6 +5,7 @@ fi
 
 export CPPFLAGS="-I$COSIM_PATH/lib"
 export LDFLAGS="-L$COSIM_PATH/lib/simbricks/nicif/ -lnicif -L$COSIM_PATH/lib/simbricks/network/ -lnetwork -L$COSIM_PATH/lib/simbricks/base/ -lbase"
+export CXXFLAGS="-Wno-range-loop-construct"
 if [ "$1" = "configure" ] ; then
   ./waf configure --enable-examples
 fi
