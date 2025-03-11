57411526716cdff6b17718ba2eeef9020653f8ed
Hejing Li <hajeongee@gmail.com>
2025-03-11 08:40:58 +0000

    fix host index

--- src/mpi/examples/dc_fat_sim.cc
+++ src/mpi/examples/dc_fat_sim.cc
@@ -240,23 +240,23 @@ int main (int argc, char *argv[])
 				int agg_idx = r % racks_per_pod;
 				if (internal_traffic){
 					// the first host in rack as the sink and the rest of it as clients 
-					sink(edgei[pod_idx][agg_idx][0].GetAddress(1), edge[pod_idx][agg_idx][0].Get(1));
-					Simulator::Schedule(Seconds(END), &PrintSinkRx, pod_idx, agg_idx, 0, DynamicCast<PacketSink>(edge[pod_idx][agg_idx][0].Get(1)->GetApplication(0)));
+					sink(edgei[pod_idx][agg_idx][0].GetAddress(0), edge[pod_idx][agg_idx][0].Get(0));
+					Simulator::Schedule(Seconds(END), &PrintSinkRx, pod_idx, agg_idx, 0, DynamicCast<PacketSink>(edge[pod_idx][agg_idx][0].Get(0)->GetApplication(0)));
 
 					for (int c = 1; c < k/2; c++){
-						client(edgei[pod_idx][agg_idx][0].GetAddress(1), edge[pod_idx][agg_idx][c].Get(1));
+						client(edgei[pod_idx][agg_idx][0].GetAddress(0), edge[pod_idx][agg_idx][c].Get(0));
 					}
 				}
 				else{
 					if (agg_idx % 2){
 						// an odd rack, all hosts are clients send to sinks in the next pod
-						client(edgei[(pod_idx+1) % k][agg_idx - 1][0].GetAddress(1),edge[pod_idx][agg_idx][0].Get(1));
-						client(edgei[(pod_idx+1) % k][agg_idx - 1][1].GetAddress(1),edge[pod_idx][agg_idx][1].Get(1));
+						client(edgei[(pod_idx+1) % k][agg_idx - 1][0].GetAddress(0),edge[pod_idx][agg_idx][0].Get(0));
+						client(edgei[(pod_idx+1) % k][agg_idx - 1][1].GetAddress(0),edge[pod_idx][agg_idx][1].Get(0));
 						NS_LOG_INFO("SystemId: " << systemId << " PodIdx: " << pod_idx << " AggIdx: " << agg_idx << " Client");
 					}
 					else{
-						sink(edgei[pod_idx][agg_idx][0].GetAddress(1), edge[pod_idx][agg_idx][0].Get(1));
-						sink(edgei[pod_idx][agg_idx][1].GetAddress(1), edge[pod_idx][agg_idx][1].Get(1));
+						sink(edgei[pod_idx][agg_idx][0].GetAddress(0), edge[pod_idx][agg_idx][0].Get(0));
+						sink(edgei[pod_idx][agg_idx][1].GetAddress(0), edge[pod_idx][agg_idx][1].Get(0));
 						NS_LOG_INFO("SystemId: " << systemId << " PodIdx: " << pod_idx << " AggIdx: " << agg_idx << " Sink");
 					}
 				}
