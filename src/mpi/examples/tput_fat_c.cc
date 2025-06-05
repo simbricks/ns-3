2a96b44b2f33a5e1b08da05d2407fdd6ed83d9ff
Hejing Li <hajeongee@gmail.com>
2025-06-05 00:06:59 +0000

    shorter time

--- src/mpi/examples/tput_fat_c.cc
+++ src/mpi/examples/tput_fat_c.cc
@@ -21,10 +21,10 @@
 
 #define START 0.0
 #define END 10
-#define NUM_STEPS 20
-#define BOOT_TIME 1
+#define NUM_STEPS 100
+#define BOOT_TIME 0.2
 #define CONV_WIN 1
-#define MEASURE_WIN 5.0
+#define MEASURE_WIN 4.0
 
 // #define PCAP_ENABLE // Use Csma devices to generate pcap files
 
